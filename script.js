9bb20b626c3bc58d5733932b08a5d918c3df403f
Anshul Kumar <46952665+Anshulnorya@users.noreply.github.com>
2022-09-01 20:16:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ calculate.addEventListener('click',()=>{
   let totalBill =document.querySelector('#total-bill').value;
   let tipPercent =document.querySelector('#tip-percent').value;
   let split =document.querySelector('#split').value;
+  // Calculation 
+  let total =(totalBill*tipPercent)/split;
+  total=total.toFixed(2);
+  let tip = document.querySelector('#tip');
+  tip.innerHTML=total;
   
   // if fields are empty
 
   if(totalBill==="" || tipPercent==="" || split===""){
-    alert("Please Enter Values")
+    // alert("Please Enter Values")
+    tip.innerHTML="Please Enter Values";
   }
-// Calculation 
-let total =(totalBill*tipPercent)/split;
-total=total.toFixed(2);
-let tip = document.querySelector('#tip');
-tip.innerHTML=total;
+  if(totalBill<0||split<0){
+    // alert("Enter positive value");
+    // tip.style.fontSize="10px;"
+    tip.innerHTML="Please Enter Positive Values";
+  }
 });
 
 
@@ -22,5 +28,7 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change',()=>{
   var container = document.querySelector('.container');
  container.classList.toggle("dark");
+ var btn = document.querySelector('#calculate');
+ btn.classList.toggle("dark");
  
 })
